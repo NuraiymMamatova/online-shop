bfdc6f8458a6801872a3317223acb2c71c7abca3
Mamatova Nuraiym <mnurajym9@gmail.com>
2023-09-19 11:06:38 +0600

    i don't what i am doing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -10,4 +10,5 @@ export default defineConfig({
       svgrOptions: {},
     }),
   ],
+  base: "/vite-deploy-demo/",
 });
